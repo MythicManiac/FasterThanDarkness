cf67b393eb2b3a6ad7511bac692b626ce1aa5e2b
Mythic <mythicmaniac@gmail.com>
2018-12-01 22:56:53 +0200

    Improve the Game class
    Separate the Game class into several functions

--- src/game.js
+++ src/game.js
@@ -2,31 +2,26 @@ import * as PIXI from 'pixi.js'
 
 export class Game {
   constructor() {
-  }
-
-  run() {
-    const app = new PIXI.Application({
+    this.app = new PIXI.Application({
       width: 256,
       height: 256,
     });
+  }
 
-    document.body.appendChild(app.view);
-
+  run() {
+    document.body.appendChild(this.app.view);
     PIXI.loader
       .add("assets/ship.png")
-      .load(setup);
+      .load(this.setup.bind(this))
+  }
 
-    let ship;
+  setup() {
+    this.ship = new PIXI.Sprite(PIXI.loader.resources["assets/ship.png"].texture);
+    this.app.stage.addChild(this.ship);
+    this.app.ticker.add(delta => this.gameLoop(delta));
+  }
 
-    function setup() {
-      ship = new PIXI.Sprite(PIXI.loader.resources["assets/ship.png"].texture);
-      app.stage.addChild(ship);
-
-      app.ticker.add(delta => gameLoop(delta));
-    }
-
-    function gameLoop(delta) {
-      ship.x += 1;
-    }
+  gameLoop() {
+    this.ship.x += 1;
   }
 }
