62ea035f4ab66046b9c26dd9f741e068ee25cd21
iAlienator <joonas.muukkonen@gmail.com>
2018-12-01 22:09:46 +0200

    Add PixiJS renderer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import { component } from "./second";
-
 document.body.appendChild(component());
 
-console.log("Hello");
+import * as PIXI from 'pixi.js'
+
+const app = new PIXI.Application({
+  width: 256,
+  height: 256,
+});
+
+document.body.appendChild(app.view);
+
+PIXI.loader
+  .add("assets/ship.png")
+  .load(setup);
+
+function setup() {
+  let cat = new PIXI.Sprite(PIXI.loader.resources["assets/ship.png"].texture);
+  app.stage.addChild(cat);
+}
