2b15dd2458dfee0129446b9a9222d1392714b23a
Mythic <mythicmaniac@gmail.com>
2018-12-03 00:12:14 +0200

    Improve player spaceship compartment setup

--- src/content/playerSpaceship.js
+++ src/content/playerSpaceship.js
@@ -2,16 +2,60 @@ import { Spaceship } from "../spaceship/spaceship";
 import { Compartment } from "../spaceship/compartment";
 
 
+function getLeftRearWing() {
+  let compartment = new Compartment("Left Rear Wing", 2);
+  return compartment;
+}
+
+function getRightRearWing() {
+  let compartment = new Compartment("Right Rear Wing", 2);
+  return compartment;
+}
+
+function getLeftFrontWing() {
+  let compartment = new Compartment("Left Front Wing", 3);
+  return compartment;
+}
+
+function getRightFrontWing() {
+  let compartment = new Compartment("Right Front Wing", 3);
+  return compartment;
+}
+
+function getRear() {
+  let compartment = new Compartment("Rear", 5);
+  return compartment;
+}
+
+function getHull() {
+  let compartment = new Compartment("Hull", 7);
+  return compartment;
+}
+
+function getCargoBay() {
+  let compartment = new Compartment("Cargo Bay", 10);
+  return compartment;
+}
+
+function getCockpit() {
+  let compartment = new Compartment("Cockpit");
+  return compartment;
+}
+
+
 export class PlayerSpaceship extends Spaceship {
   constructor() {
     super();
-    this.addCompartment(new Compartment("Left Rear Wing", 2));
-    this.addCompartment(new Compartment("Right Rear Wing", 2));
-    this.addCompartment(new Compartment("Left Front Wing", 3));
-    this.addCompartment(new Compartment("Right Front Wing", 3));
-    this.addCompartment(new Compartment("Rear", 5));
-    this.addCompartment(new Compartment("Hull", 7));
-    this.addCompartment(new Compartment("Cargo Bay", 10));
-    this.addCompartment(new Compartment("Cockpit"));
+  }
+
+  initializeModules() {
+    this.addCompartment(getLeftRearWing());
+    this.addCompartment(getRightRearWing());
+    this.addCompartment(getLeftFrontWing());
+    this.addCompartment(getRightFrontWing());
+    this.addCompartment(getRear());
+    this.addCompartment(getHull());
+    this.addCompartment(getCargoBay());
+    this.addCompartment(getCockpit());
   }
 }
